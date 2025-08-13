72df1595ead043d0adde3c72222db1b24c1a3f15
Nyi Htet <nyihtetcleanslate@gmail.com>
2025-08-12 20:45:35 -0700

    Basic core functionalities for Lobby View are added.

---
+++ ViewControllers/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  Who's da Boss
+//
+//  Created by Nyi Htet on 8/9/25.
+//
+
+import Foundation
---
+++ Views/LobbyView.swift
@@ -0,0 +1,18 @@
+//
+//  LobbyView.swift
+//  Who's da Boss
+//
+//  Created by Nyi Htet on 8/9/25.
+//
+
+import SwiftUI
+
+struct LobbyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LobbyView()
+}
